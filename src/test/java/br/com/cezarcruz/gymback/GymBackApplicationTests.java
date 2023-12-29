113081d8b54761e0ce4c5e94dbaa40fad3901917
Cezar Cruz <cezarcruz22@gmail.com>
2023-12-29 11:09:37 -0300

    fix test container

--- src/test/java/br/com/cezarcruz/gymback/GymBackApplicationTests.java
+++ src/test/java/br/com/cezarcruz/gymback/GymBackApplicationTests.java
@@ -2,9 +2,14 @@ package br.com.cezarcruz.gymback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 class GymBackApplicationTests {
+
+	@Container
+	private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>("mysql:latest");
 
 	@Test
 	void contextLoads() {
