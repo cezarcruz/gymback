5f9a05df38c9690e773c3b93d199701f5df93db2
Cezar Cruz <cezarcruz22@gmail.com>
2024-01-07 19:57:27 -0300

    removing gradle

---
+++ target/generated-sources/annotations/br/com/cezarcruz/gymback/application/rest/mapper/AddressMapperImpl.java
@@ -0,0 +1,63 @@
+package br.com.cezarcruz.gymback.application.rest.mapper;
+
+import br.com.cezarcruz.gymback.application.rest.dto.request.CreateAddressRequest;
+import br.com.cezarcruz.gymback.application.rest.dto.response.AddressResponse;
+import br.com.cezarcruz.gymback.core.domain.AddressDomain;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-07T19:55:05-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21 (Eclipse Adoptium)"
+)
+@Singleton
+@Named
+public class AddressMapperImpl implements AddressMapper {
+
+    @Override
+    public AddressDomain from(CreateAddressRequest source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        AddressDomain.AddressDomainBuilder addressDomain = AddressDomain.builder();
+
+        addressDomain.addressNumber( source.num() );
+        addressDomain.zipcode( source.zipcode() );
+        addressDomain.street( source.street() );
+        addressDomain.state( source.state() );
+        addressDomain.city( source.city() );
+        addressDomain.neighborhood( source.neighborhood() );
+
+        return addressDomain.build();
+    }
+
+    @Override
+    public AddressResponse from(AddressDomain source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        String id = null;
+        String zipcode = null;
+        String street = null;
+        String neighborhood = null;
+        String addressNumber = null;
+        String state = null;
+        String city = null;
+
+        id = source.id();
+        zipcode = source.zipcode();
+        street = source.street();
+        neighborhood = source.neighborhood();
+        addressNumber = source.addressNumber();
+        state = source.state();
+        city = source.city();
+
+        AddressResponse addressResponse = new AddressResponse( id, zipcode, street, neighborhood, addressNumber, state, city );
+
+        return addressResponse;
+    }
+}
---
+++ target/generated-sources/annotations/br/com/cezarcruz/gymback/application/rest/mapper/ModalityMapperImpl.java
@@ -0,0 +1,65 @@
+package br.com.cezarcruz.gymback.application.rest.mapper;
+
+import br.com.cezarcruz.gymback.application.rest.dto.request.CreateModalityRequest;
+import br.com.cezarcruz.gymback.application.rest.dto.request.UpdateModalityRequest;
+import br.com.cezarcruz.gymback.application.rest.dto.response.ModalityResponse;
+import br.com.cezarcruz.gymback.core.domain.ModalityDomain;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-07T19:55:05-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21 (Eclipse Adoptium)"
+)
+@Singleton
+@Named
+public class ModalityMapperImpl implements ModalityMapper {
+
+    @Override
+    public ModalityDomain toModality(CreateModalityRequest createModalityRequest) {
+        if ( createModalityRequest == null ) {
+            return null;
+        }
+
+        ModalityDomain.ModalityDomainBuilder modalityDomain = ModalityDomain.builder();
+
+        modalityDomain.name( createModalityRequest.name() );
+
+        return modalityDomain.build();
+    }
+
+    @Override
+    public ModalityResponse from(ModalityDomain createdModality) {
+        if ( createdModality == null ) {
+            return null;
+        }
+
+        String id = null;
+        String name = null;
+
+        id = createdModality.id();
+        name = createdModality.name();
+
+        ModalityResponse modalityResponse = new ModalityResponse( id, name );
+
+        return modalityResponse;
+    }
+
+    @Override
+    public ModalityDomain toModality(String id, UpdateModalityRequest updateModalityRequest) {
+        if ( id == null && updateModalityRequest == null ) {
+            return null;
+        }
+
+        ModalityDomain.ModalityDomainBuilder modalityDomain = ModalityDomain.builder();
+
+        if ( updateModalityRequest != null ) {
+            modalityDomain.name( updateModalityRequest.name() );
+        }
+        modalityDomain.id( id );
+
+        return modalityDomain.build();
+    }
+}
