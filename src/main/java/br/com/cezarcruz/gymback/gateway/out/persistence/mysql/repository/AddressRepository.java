aee2d05e45d5bf2c438834718e40c0576fd112aa
Cezar Cruz <cezarcruz22@gmail.com>
2024-01-07 09:48:11 -0300

    fix id

--- src/main/java/br/com/cezarcruz/gymback/gateway/out/persistence/mysql/repository/AddressRepository.java
+++ src/main/java/br/com/cezarcruz/gymback/gateway/out/persistence/mysql/repository/AddressRepository.java
@@ -1,10 +1,11 @@
 package br.com.cezarcruz.gymback.gateway.out.persistence.mysql.repository;
 
-import br.com.cezarcruz.gymback.gateway.out.persistence.mysql.entity.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.cezarcruz.gymback.gateway.out.persistence.mysql.entity.AddressEntity;
+
 @Repository
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
+public interface AddressRepository extends JpaRepository<AddressEntity, String> {
 
 }
