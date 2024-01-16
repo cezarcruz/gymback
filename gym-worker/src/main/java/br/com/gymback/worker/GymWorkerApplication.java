87f20bd124065a97cb085e7b64787ce063e128ff
Cezar Cruz <cezarcruz22@gmail.com>
2024-01-16 14:38:55 -0300

    adding worker

---
+++ gym-worker/src/main/java/br/com/gymback/worker/GymWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gymback.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWorkerApplication {
+
+  public static void main(final String...args) {
+    SpringApplication.run(GymWorkerApplication.class, args);
+  }
+
+}
