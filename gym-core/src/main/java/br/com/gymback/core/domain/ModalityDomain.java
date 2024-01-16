df80cb5a24be0e30b212607e152cabe076cbb7e4
cezarcruz <cezarcruz@users.noreply.github.com>
2024-01-16 12:45:58 +0000

    Commit from GitHub Actions (Build Java)

--- gym-core/src/main/java/br/com/gymback/core/domain/ModalityDomain.java
+++ gym-core/src/main/java/br/com/gymback/core/domain/ModalityDomain.java
@@ -1,13 +1,7 @@
+/* Under MIT License (C)2024 */
 package br.com.gymback.core.domain;
 
 import lombok.Builder;
 
 @Builder
-public record ModalityDomain(
-    Long id,
-    String name
-) implements GenericDomain {
-
-}
-
-
+public record ModalityDomain(Long id, String name) implements GenericDomain {}
