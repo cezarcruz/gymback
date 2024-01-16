6f338afc66e5e3e336174812dedce7b9a102e886
Cezar Cruz <cezarcruz22@gmail.com>
2024-01-16 09:22:44 -0300

    using assertj

--- gym-core/src/test/java/br/com/gymback/core/usecase/classroom/CreateClassRoomUseCaseTest.java
+++ gym-core/src/test/java/br/com/gymback/core/usecase/classroom/CreateClassRoomUseCaseTest.java
@@ -1,13 +1,15 @@
+/* Under MIT License (C)2024 */
 package br.com.gymback.core.usecase.classroom;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
+import br.com.gymback.core.domain.ClassRoomDomain;
+import br.com.gymback.core.exceptions.TeacherNotFountException;
 import br.com.gymback.core.fixtures.ClassRoomFixtures;
-import br.com.gymback.core.exceptions.NotFoundException;
 import br.com.gymback.core.gateway.classroom.SaveClassRoomGateway;
 import br.com.gymback.core.gateway.modality.GetModalityGateway;
 import br.com.gymback.core.gateway.teacher.GetTeacherGateway;
@@ -21,28 +23,26 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class CreateClassRoomUseCaseTest {
 
-  @Mock
-  private GetTeacherGateway getTeacherGateway;
+  @Mock private GetTeacherGateway getTeacherGateway;
 
-  @Mock
-  private SaveClassRoomGateway saveClassRoomGateway;
+  @Mock private SaveClassRoomGateway saveClassRoomGateway;
 
-  @Mock
-  private GetModalityGateway getModalityGateway;
+  @Mock private GetModalityGateway getModalityGateway;
 
-  @InjectMocks
-  private CreateClassRoomUseCase createClassRoomUseCase;
+  @InjectMocks private CreateClassRoomUseCase createClassRoomUseCase;
 
   @Test
   void shouldNotFindTeacher() {
-    when(getTeacherGateway.findById(any()))
-        .thenReturn(Optional.empty());
+    when(getTeacherGateway.findById(any())).thenReturn(Optional.empty());
 
-    assertThrows(NotFoundException.class, () -> createClassRoomUseCase.create(ClassRoomFixtures.getClassRoom()));
+    final ClassRoomDomain classRoom = ClassRoomFixtures.getClassRoom();
+
+    assertThatThrownBy(() -> createClassRoomUseCase.create(classRoom))
+        .hasMessage(String.format("teacher %s not found", classRoom.teacher().id()))
+        .isInstanceOf(TeacherNotFountException.class);
 
     verify(getTeacherGateway).findById(any());
     verifyNoInteractions(getModalityGateway);
     verifyNoInteractions(saveClassRoomGateway);
   }
-
 }
