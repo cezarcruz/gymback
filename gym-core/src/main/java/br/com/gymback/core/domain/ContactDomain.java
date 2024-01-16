78fc8c0b5399f0427427045bf3f0e3a9c7a8667e
Cezar Cruz <cezarcruz@outlook.com>
2024-01-16 12:52:47 +0000

    applying google style guide

--- gym-core/src/main/java/br/com/gymback/core/domain/ContactDomain.java
+++ gym-core/src/main/java/br/com/gymback/core/domain/ContactDomain.java
@@ -1,10 +1,4 @@
+/* Under MIT License (C)2024 */
 package br.com.gymback.core.domain;
 
-public record ContactDomain(
-    Long id,
-    String phone,
-    String email
-) {
-
-
-}
+public record ContactDomain(Long id, String phone, String email) {}
