12e3ca51e61a4ddd1f9a48f9cf20c32353f8d976
Cezar Cruz <cezarcruz22@gmail.com>
2024-01-17 17:23:03 -0300

    update

--- gym-core/src/main/java/br/com/gymback/core/usecase/teacher/CreateTeacherUseCase.java
+++ gym-core/src/main/java/br/com/gymback/core/usecase/teacher/CreateTeacherUseCase.java
@@ -5,14 +5,18 @@ import br.com.gymback.core.domain.TeacherDomain;
 import br.com.gymback.core.gateway.contact.SaveContactGateway;
 import br.com.gymback.core.gateway.teacher.SaveTeacherGateway;
 import jakarta.inject.Named;
-import lombok.RequiredArgsConstructor;
 
 @Named
-@RequiredArgsConstructor
 public class CreateTeacherUseCase {
 
   private final SaveTeacherGateway saveTeacherGateway;
   private final SaveContactGateway saveContactGateway;
+
+  public CreateTeacherUseCase(final SaveTeacherGateway saveTeacherGateway,
+      final SaveContactGateway saveContactGateway) {
+    this.saveTeacherGateway = saveTeacherGateway;
+    this.saveContactGateway = saveContactGateway;
+  }
 
   public TeacherDomain create(final TeacherDomain teacher) {
     var contacts = saveContactGateway.save(teacher.contact());
--- gym-core/src/main/java/br/com/gymback/core/usecase/teacher/UpdateTeacherUseCase.java
+++ gym-core/src/main/java/br/com/gymback/core/usecase/teacher/UpdateTeacherUseCase.java
@@ -5,16 +5,20 @@ import br.com.gymback.core.exceptions.TeacherNotFountException;
 import br.com.gymback.core.gateway.teacher.GetTeacherGateway;
 import br.com.gymback.core.gateway.teacher.SaveTeacherGateway;
 import jakarta.inject.Named;
-import lombok.RequiredArgsConstructor;
 
 @Named
-@RequiredArgsConstructor
 public class UpdateTeacherUseCase {
 
     private final SaveTeacherGateway saveTeacherGateway;
     private final GetTeacherGateway getTeacherGateway;
 
-    public TeacherDomain update(final TeacherDomain teacher) {
+  public UpdateTeacherUseCase(SaveTeacherGateway saveTeacherGateway,
+      GetTeacherGateway getTeacherGateway) {
+    this.saveTeacherGateway = saveTeacherGateway;
+    this.getTeacherGateway = getTeacherGateway;
+  }
+
+  public TeacherDomain update(final TeacherDomain teacher) {
 
         return getTeacherGateway
             .findById(teacher.id())
