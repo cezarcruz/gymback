f9eb6b7cbbb6a21e3b1eb0d74f1c4ebc2a0fa216
Cezar Cruz <cezarcruz22@gmail.com>
2024-01-18 16:37:58 -0300

    increment tests coverage

---
+++ gym-core/src/test/java/br/com/gymback/core/usecase/payment/UpdateOverduePaymentUseCaseTest.java
@@ -0,0 +1,48 @@
+package br.com.gymback.core.usecase.payment;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import br.com.gymback.core.domain.PaymentDomain;
+import br.com.gymback.core.enums.PaymentStatus;
+import br.com.gymback.core.fixtures.PaymentFixture;
+import br.com.gymback.core.gateway.payment.GetPaymentGateway;
+import br.com.gymback.core.gateway.payment.SavePaymentGateway;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateOverduePaymentUseCaseTest {
+
+  @Mock
+  private GetPaymentGateway getPaymentGateway;
+
+  @Mock
+  private SavePaymentGateway savePaymentGateway;
+
+  @InjectMocks
+  private UpdateOverduePaymentUseCase updateOverduePaymentUseCase;
+
+  @Test
+  void shouldUpdateAllOverduePayments() {
+
+    final var payments = List.of(PaymentFixture.getPayment());
+    when(getPaymentGateway.findAllBy(PaymentStatus.PENDING, LocalDate.now()))
+        .thenReturn(payments);
+
+    updateOverduePaymentUseCase.execute();
+
+    verify(savePaymentGateway).saveAll(argThat(a -> {
+      var payment = a.getFirst();
+      return payment.getPaymentStatus().equals(PaymentStatus.OVERDUE);
+    }));
+  }
+
+}
