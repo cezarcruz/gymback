f65899525aee5e6ac300fd052e4f41082e9c1b3c
Cezar Cruz <cezarcruz22@gmail.com>
2024-01-18 22:41:50 -0300

    putting bruno here, just for get some new idea late

--- gym-application/src/main/java/br/com/gymback/application/rest/dto/request/CreateStudentRequest.java
+++ gym-application/src/main/java/br/com/gymback/application/rest/dto/request/CreateStudentRequest.java
@@ -2,12 +2,13 @@ package br.com.gymback.application.rest.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record CreateStudentRequest(
 
-    @NotNull String name,
+    @NotNull @NotBlank String name,
     @NotNull @JsonFormat(pattern = "dd/MM/yyyy") LocalDate birthDate,
     String document,
     @Valid @NotNull CreateAddressRequest address,
