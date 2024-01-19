249dd39476c02933465f19469c126d7e47661ca8
Cezar Cruz <cezarcruz@outlook.com>
2024-01-19 01:44:39 +0000

    applying google style guide

--- gym-application/src/main/java/br/com/gymback/application/rest/dto/request/CreateStudentRequest.java
+++ gym-application/src/main/java/br/com/gymback/application/rest/dto/request/CreateStudentRequest.java
@@ -1,3 +1,4 @@
+/* Under MIT License (C)2024 */
 package br.com.gymback.application.rest.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -7,14 +8,8 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record CreateStudentRequest(
-
     @NotNull @NotBlank String name,
     @NotNull @JsonFormat(pattern = "dd/MM/yyyy") LocalDate birthDate,
     String document,
     @Valid @NotNull CreateAddressRequest address,
-
-    @Valid @NotNull CreateContactRequest contact
-
-) {
-
-}
+    @Valid @NotNull CreateContactRequest contact) {}
