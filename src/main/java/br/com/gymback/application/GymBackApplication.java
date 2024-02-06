a777c58b6a0dce02063eecb652d22eca3b0d0d66
Cezar Cruz <cezarcruz22@gmail.com>
2024-02-06 14:03:01 -0300

    removing main

--- src/main/java/br/com/gymback/application/GymBackApplication.java
+++ src/main/java/br/com/gymback/application/GymBackApplication.java
@@ -1,10 +1,17 @@
 package br.com.gymback.application;
 
+import br.com.gymback.worker.config.WorkerContextConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableKafka
+@EnableScheduling
+@Import(WorkerContextConfig.class)
 public class GymBackApplication {
   public static void main(final String... args) {
     SpringApplication.run(GymBackApplication.class, args);
