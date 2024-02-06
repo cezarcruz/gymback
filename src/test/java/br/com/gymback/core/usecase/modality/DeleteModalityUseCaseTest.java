c3f98adf2f29adef0c6145b522f4ad042e391369
Cezar Cruz <cezarcruz@users.noreply.github.com>
2024-02-06 20:16:16 +0000

    applying google style guide

--- src/test/java/br/com/gymback/core/usecase/modality/DeleteModalityUseCaseTest.java
+++ src/test/java/br/com/gymback/core/usecase/modality/DeleteModalityUseCaseTest.java
@@ -1,3 +1,4 @@
+/* Under MIT License (C)2024 */
 package br.com.gymback.core.usecase.modality;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,11 +14,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class DeleteModalityUseCaseTest {
 
-  @Mock
-  private DeleteModalityGateway deleteModalityGateway;
+  @Mock private DeleteModalityGateway deleteModalityGateway;
 
-  @InjectMocks
-  private DeleteModalityUseCase deleteModalityUseCase;
+  @InjectMocks private DeleteModalityUseCase deleteModalityUseCase;
 
   @Test
   void shouldDeleteModality() {
@@ -25,5 +24,4 @@ class DeleteModalityUseCaseTest {
 
     verify(deleteModalityGateway).deleteById(1L);
   }
-
 }
