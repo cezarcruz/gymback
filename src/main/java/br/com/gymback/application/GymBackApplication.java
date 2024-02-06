d0a5c9da5b9790349b94147dbd20610042bc440a
Cezar Cruz <cezarcruz22@gmail.com>
2024-02-06 14:17:37 -0300

    fixing context

--- src/main/java/br/com/gymback/application/GymBackApplication.java
+++ src/main/java/br/com/gymback/application/GymBackApplication.java
@@ -1,6 +1,7 @@
 /* Under MIT License (C)2024 */
 package br.com.gymback.application;
 
+import br.com.gymback.application.config.context.ApplicationContextConfig;
 import br.com.gymback.worker.config.WorkerContextConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableKafka
 @EnableScheduling
-@Import(WorkerContextConfig.class)
+@Import({WorkerContextConfig.class, ApplicationContextConfig.class})
 public class GymBackApplication {
   public static void main(final String... args) {
     SpringApplication.run(GymBackApplication.class, args);
--- src/main/java/br/com/gymback/messaging/config/MessagingContextConfig.java
+++ src/main/java/br/com/gymback/messaging/config/MessagingContextConfig.java
@@ -5,5 +5,5 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "br.com.messaging.out.kafka")
+@ComponentScan(basePackages = "br.com.gymback.messaging.out.kafka")
 public class MessagingContextConfig {}
