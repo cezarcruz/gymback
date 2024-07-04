5c38d0310219f380a759776f4597c2e51b670292
Cezar Cruz <cezarcruz22@gmail.com>
2024-07-04 09:04:02 -0300

    fixing error on teacher controller

--- src/main/java/br/com/gymback/application/rest/dto/request/CreateTeacherRequest.java
+++ src/main/java/br/com/gymback/application/rest/dto/request/CreateTeacherRequest.java
@@ -1,8 +1,13 @@
 /* Under MIT License (C)2024 */
 package br.com.gymback.application.rest.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record CreateTeacherRequest(
-    @NotNull String name, LocalDate birthDate, CreateContactRequest contact) {}
+    @NotNull String name,
+    LocalDate birthDate,
+    @Valid
+    @NotNull
+    CreateContactRequest contact) {}
